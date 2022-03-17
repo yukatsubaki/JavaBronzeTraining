5e2538e5540ac87937fedb7d7555bdc4a184c935
yukatsubaki <icdeuty4jb2@gmail.com>
2022-03-17 23:55:59 +0900

    test1

---
+++ com/se/Sample8_7.java
@@ -0,0 +1,14 @@
+package com.se;
+
+class Foo{
+	void print(){
+		System.out.println("package sample");
+	}
+}
+
+class Sample8_7{
+	public static void main(String[] args){
+		Foo f = new Foo();
+		f.print();
+	}
+}
